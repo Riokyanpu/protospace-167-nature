5077590a5f609a6fce2a4e321a420123da68cddf
cookiemonster150 <160557231+cookiemonster150@users.noreply.github.com>
2024-03-10 15:59:21 +0900

    バリデーションを設定しましょう

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,4 +2,14 @@ class Prototype < ApplicationRecord
   belong_to :user
   has_many :comments
   has_one_attached :image
+
+  validates :title, presence: true
+  validates :catch_copy, presence: true
+  validates :concept, presence: true
+  validates :content, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
+
 end
