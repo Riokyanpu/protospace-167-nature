d5f49e446def43e6bdd2c47ea85625599ed33862
cookiemonster150 <160557231+cookiemonster150@users.noreply.github.com>
2024-03-10 14:13:26 +0900

    Revert "prototypeモデルおよびテーブルを作成"
    This reverts commit 261fc4f5272f3f2e0214efa2928355920ae3f5e8.

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_09_064014) do
-  create_table "prototypes", charset: "utf8", force: :cascade do |t|
-    t.string "title", null: false
-    t.text "catch_copy", null: false
-    t.text "concept", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_prototypes_on_user_id"
-  end
-
+ActiveRecord::Schema[7.0].define(version: 2024_03_08_063540) do
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -37,5 +27,4 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_09_064014) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "prototypes", "users"
 end
