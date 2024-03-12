3fa1511bc1d98ad136f98b77ece2d38a65e85617
ichikuwu <q08012@gmail.com>
2024-03-12 14:06:21 +0900

    DB作成カラム名作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_08_063540) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_11_023028) do
+  create_table "comments", charset: "utf8", force: :cascade do |t|
+    t.text "content_id"
+    t.bigint "prototype_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["prototype_id"], name: "index_comments_on_prototype_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
