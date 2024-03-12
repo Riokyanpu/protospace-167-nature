f41f0c0a9439bb28cd6de69013aa8748ff08e469
ichikuwu <q08012@gmail.com>
2024-03-12 14:06:44 +0900

    ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :user
   root to: "prototypes#index"
-   resources :prototypes, only: :new 
+   resources :prototypes, only: :new do
+     resources :comments, only: :create
+   end  
 end
