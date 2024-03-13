9bc59daf74c794f445630903c2d78f6ff35aea63
Riokyanpu <kgst1216@gmail.com>
2024-03-13 20:19:47 +0900

    ユーザー詳細編集

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,6 @@
 class PrototypesController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :show]
+  before_action :move_to_index, only: [:edit]
   def index
   end
 
@@ -18,7 +20,13 @@ class PrototypesController < ApplicationController
   private
 
   def prototype_params
-    params.require(:prototype).permit(:title,:catch_copy,:concept,:image).merge(user_id: current_user.id)
- end
-end
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
 
+  def move_to_index
+    @prototype = Prototype.find(params[:id])
+    return unless current_user != @prototype.user
+
+    redirect_to action: :index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :user
   root to: "prototypes#index"
    resources :prototypes, only: [:new,:create] 
+   resources :users, only: [:show] 
 end
