d26ed5575dd85c1400f69bb0d031e7730c2b15a6
Riokyanpu <kgst1216@gmail.com>
2024-03-14 11:17:15 +0900

    ロクアウト状態でもトップページに遷移

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,5 @@
 class PrototypesController < ApplicationController
-
-  before_action :authenticate_user!, only: [:index, :show]
+  # before_action :authenticate_user!, only: [:index, :show]
   before_action :move_to_index, only: [:edit]
 
   def index
@@ -52,12 +51,10 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
-
   def move_to_index
     @prototype = Prototype.find(params[:id])
     return unless current_user != @prototype.user
 
     redirect_to action: :index
-
   end
 end
