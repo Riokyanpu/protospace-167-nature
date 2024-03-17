336d8a7a0dd22a779dad49e89d4121511089f509
cookiemonster150 <160557231+cookiemonster150@users.noreply.github.com>
2024-03-17 18:37:58 +0900

    修正4

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,16 +2,12 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      redirect_to prototype_comments_path(@comment.prototype) # 今回の実装には関係ありませんが、このようにPrefixでパスを指定することが望ましいです。
+      redirect_to prototype_path(@comment.prototype) # 今回の実装には関係ありませんが、このようにPrefixでパスを指定することが望ましいです。
     else
       @prototype = @comment.prototype
       @comments = @prototype.comments
       render "prototypes/show"
     end
-  end
-
-  def show
-    @comment = comments.all
   end
 
   private
